1cf472ad74ffff7b2790abf5b622d78690024d6c
tiaxter <jerrytiapalmiotto@gmail.com>
2022-10-26 00:49:24 +0200

    feat: import and use weather router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import express, {type Request, type Response} from "express";
+import dotenv from "dotenv";
+import express, {type Request, type Response, type NextFunction} from "express";
+import weatherRouter from "./routers/weather";
+
+dotenv.config({
+  path: `${__dirname}/../.env`,
+});
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send('Hello World');
-});
+app.use("/api", [
+  weatherRouter(),
+]);
 
 app.listen(3000, () => console.log('App is listening on port 3000'));
