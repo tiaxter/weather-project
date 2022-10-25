b08f3e75b12039824c64b26aa7c22e852b2e8be2
tiaxter <jerrytiapalmiotto@gmail.com>
2022-10-26 01:25:09 +0200

    feat: import business router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, {type Request, type Response, type NextFunction} from "express";
 import weatherRouter from "./routers/weather";
+import businessRouter from "./routers/business";
 
 dotenv.config({
   path: `${__dirname}/../.env`,
@@ -10,6 +11,7 @@ const app = express();
 
 app.use("/api", [
   weatherRouter(),
+  businessRouter(),
 ]);
 
 app.listen(3000, () => console.log('App is listening on port 3000'));
