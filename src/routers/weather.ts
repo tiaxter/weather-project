b6bd32e728de863452eaf6405662d8eb28012a0c
tiaxter <jerrytiapalmiotto@gmail.com>
2022-10-26 13:38:57 +0200

    feat: add endpoint to the weather router

--- src/routers/weather.ts
+++ src/routers/weather.ts
@@ -1,5 +1,5 @@
 import {Router, type Response, type Request} from "express";
-import {getWeatherByCity} from "../services/weather";
+import {getWeatherByCity, getWeatherWithCityData} from "../services/weather";
 
 export default function weatherRouter() {
   const router = Router();
@@ -18,6 +18,21 @@ export default function weatherRouter() {
       res.status(500).json({ error: "Ops... An error ocurred, please retry later" });
     }
   });
+
+  router.get('/weather-with-business', async (req: Request, res: Response) => {
+    // get city name from query params
+    const cityName = req.query.city as string;
+
+    try {
+      // make request to get weather and businesses data
+      const weatherWithBusinessInfo = await getWeatherWithCityData(cityName);
+      // return data
+      res.status(200).json(weatherWithBusinessInfo);
+    } catch (e) {
+      // catch the error
+      res.status(500).json({ error: "Ops... An error ocurred, please retry later" });
+    }
+  })
 
   return router;
 }
