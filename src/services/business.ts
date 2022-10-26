1670a2423025d63a7acb007c1c0c08a5e63a87e9
tiaxter <jerrytiapalmiotto@gmail.com>
2022-10-26 11:18:42 +0200

    feat: map data structure with only useful fields

--- src/services/business.ts
+++ src/services/business.ts
@@ -1,14 +1,28 @@
 import axios from "axios";
 
+type Category = {
+  title: string,
+  alias: string,
+}
+
 export const getBusinessByCity = async (cityName: string) => {
   const ENDPOINT = `https://api.yelp.com/v3/businesses/search`;
   // make the request to yelp api
-  return (await axios.get(ENDPOINT, {
+  const business = (await axios.get(ENDPOINT, {
     headers: {
       Authorization: `Bearer ${process.env.YELP_API_KEY}`,
     },
     params: {
       location: cityName,
+      categories: 'shopping',
     }
   })).data;
+
+  return business["businesses"].map((item: any) => ({
+    "yelpId": item.id,
+    "name": item.name,
+    "imageUrl": item.image_url,
+    "address": item.location?.display_address?.[0] ?? '',
+    "categories": (item?.categories ?? []).map((category: Category) => category.title)
+  }));
 }
